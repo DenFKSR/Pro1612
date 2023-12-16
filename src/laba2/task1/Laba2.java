e161e4a8e6be11f9123dcd36f5c99dcde62a902b
Den <zuev.den10@mail.ru>
2023-12-16 21:08:13 +0300

    add laba1

---
+++ src/laba2/task1/Laba2.java
@@ -0,0 +1,13 @@
+package laba2.task1;
+
+public class Laba2 {
+    public static void main(String[] args) {
+        var a = 10000000000l;
+        var b = 300000000l;
+        System.out.println(Calculator.summ(a,b));
+        System.out.println(Calculator.subtraction(a,b));
+        System.out.println(Calculator.multiplication(a,b));
+        System.out.println(Calculator.div(a,b));
+    }
+}
+
