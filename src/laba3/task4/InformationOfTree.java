4682b974df8711e728a1badd1d2afbb01a095bfd
Den <zuev.den10@mail.ru>
2023-12-17 22:15:53 +0300

    Revert "add lab#3"
    This reverts commit 872bf8b60236723a72743fe194f977008f4fa2e1.

--- src/laba3/task4/InformationOfTree.java
+++
@@ -1,9 +0,0 @@
-package laba3.task4;
-
-public class InformationOfTree {
-    public static void main(String[] args) {
-        Tree tree1 = new Tree();
-        Tree tree2 = new Tree(12, "Дуб");
-        Tree tree3 = new Tree(20, "Осина", false);
-    }
-}
